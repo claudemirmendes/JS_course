e7fdb41c7aed40e1445705999febc88881694be8
claudemirmendes <avelinopopping@gmail.com>
2018-05-03 14:36:15 -0300

    adding tests archives and dependences for tests

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,29 @@
 import { expect } from 'chai';
+import { search,searchArtists, searchTracks, searchPlaylist} from '../src/main.js'
 
-describe('Main', () => {
+describe('Spotify', () => {
+  describe('smoke tests',() => {
+  	it('should exist search method', () => {
+      expect(search).to.exist
+  	});
 
+  	it('should exist searchArtists method', () => {
+      expect(searchArtists).to.exist
+  	});
+
+  	it('should exist searchTracks method', () => {
+      expect(searchTracks).to.exist
+  	});
+
+  	it('should exist searchPlaylist method', () => {
+      expect(searchPlaylist).to.exist
+  	});
+
+  });
+});
+
+describe ('Generic search', () => {
+  it('should call fetch function', () => {
+  	
+  })
 });
