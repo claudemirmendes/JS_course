714d105bd5e5b9023e9b9258c3e97acc8359c0b8
claudemirmendes <avelinopopping@gmail.com>
2018-05-04 09:06:38 -0300

    solved error ''fetch' is not defined'

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 export const search = (query, artist) => {
   fetch(`https://api.spotify.com/v1/search?q=${query}&type=${artist}`);
+};
 
 export const searchArtists = () => {};
 export const searchTracks = () => {};
